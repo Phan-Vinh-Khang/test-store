15ed0038fc790e7cfcbd8b7006533c6eaae0645e
Phan Vĩnh Khang <khangpv.19th@sv.dla.edu.vn>
2023-04-11 08:50:08 +0700

    commit 8:50AM, 11/4/2023

--- src/pages/login/Sign-up/index.js
+++ src/pages/login/Sign-up/index.js
@@ -1,53 +1,120 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import objStyle from './index.module.scss'
 import classNames from 'classnames/bind'
-function SignUp(obj) {
+import resLogin from '../../../services/userServices';
+function SignIn(obj) {
     let cv = classNames.bind(objStyle)
     let [state, setState] = useState({
-        userName: '',
-        password: ''
-    })
-    const getValue1 = (e) => {
-        // setState(refpreState => ({ ...refpreState, userName: e.target.value }))
-        setState({
-            userName: e.target.value,
-            password: state.password
-        })
-    }
-    const getValue2 = (e) => {
-        // setState(refpretate => ({ ...refpreState, password: e.target.value }))
-        setState({
-            userName: state.userName,
-            password: e.target.value
-        })
-    }
-    const submitAction = () => {
-        console.log(state)
-    }
-    console.log(state)
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        address: '',
+        phonenumber: ''
+    });
+    let [stateSignupMessage, setStateSignupMessage] = useState('');
+    const getValue = (e, label) => {
+        if (label == 'name') {
+            setState(refpreState => ({
+                ...refpreState,
+                name: e.target.value
+            }))
+        }
+        else if (label == 'email') {
+            setState(refpreState => ({
+                ...refpreState,
+                email: e.target.value
+            }))
+        }
+        else if (label == 'password') {
+            setState(refpreState => ({
+                ...refpreState,
+                password: e.target.value
+            }))
+        }
+        else if (label == 'confirmPassword') {
+            setState(refpreState => ({
+                ...refpreState,
+                confirmPassword: e.target.value
+            }))
+        }
+        else if (label == 'address') {
+            setState(refpreState => ({
+                ...refpreState,
+                address: e.target.value
+            }))
+        }
+        else {
+            setState(refpreState => ({
+                ...refpreState,
+                phonenumber: e.target.value
+            }))
+        }
 
+    }
+    const submitAction = async () => {
+        let data = {};
+        data = await resLogin(state.email, state.password)
+        //return về 1 obj
+        //{data: Array(10), status: 200, statusText: '', headers: AxiosHeaders, config: {…}, …}config: {transitional: {…}, adapter: Array(2), transformRequest: Array(1), transformResponse: Array(1), timeout: 0, …}data: Array(10)0: {id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz', address: {…}, …}1: {id: 2, name: 'Ervin Howell', username: 'Antonette', email: 'Shanna@melissa.tv', address: {…}, …}2: {id: 3, name: 'Clementine Bauch', username: 'Samantha', email: 'Nathan@yesenia.net', address: {…}, …}3: {id: 4, name: 'Patricia Lebsack', username: 'Karianne', email: 'Julianne.OConner@kory.org', address: {…}, …}4: {id: 5, name: 'Chelsey Dietrich', username: 'Kamren', email: 'Lucio_Hettinger@annie.ca', address: {…}, …}5: {id: 6, name: 'Mrs. Dennis Schulist', username: 'Leopoldo_Corkery', email: 'Karley_Dach@jasper.info', address: {…}, …}6: {id: 7, name: 'Kurtis Weissnat', username: 'Elwyn.Skiles', email: 'Telly.Hoeger@billy.biz', address: {…}, …}7: {id: 8, name: 'Nicholas Runolfsdottir V', username: 'Maxime_Nienow', email: 'Sherwood@rosamond.me', address: {…}, …}8: {id: 9, name: 'Glenna Reichert', username: 'Delphine', email: 'Chaim_McDermott@dana.io', address: {…}, …}9: {id: 10, name: 'Clementina DuBuque', username: 'Moriah.Stanton', email: 'Rey.Padberg@karina.biz', address: {…}, …}length: 10[[Prototype]]: Array(0)headers: AxiosHeaders {cache-control: 'max-age=43200', content-type: 'application/json; charset=utf-8', expires: '-1', pragma: 'no-cache'}request: XMLHttpRequest {onreadystatechange: null, readyState: 4, timeout: 0, withCredentials: false, upload: XMLHttpRequestUpload, …}status: 200statusText: ""[[Prototype]]: Object
+        //properties ref to static data(propertie data ref to arr,status ref to 200, config ref to obj)
+        //respon của devtool sẽ hien thị obj.data
+        console.log(data.data)
+        setStateSignupMessage(data.data.message)
+    }
     return (
         <div className={cv('form-Login')}> {/*40% width block*/}
             <div className={cv('wrapper-Login-center')}>
                 <div className={cv('wrapper-Login-label')}>
                     <label>Đăng ký</label>
-                    <div>
-                        <label>Đăng với QR</label>
-                        <a href="/"><label>QR</label></a>
-                    </div>
                 </div>
                 <div className={cv('wrapperForm')}>
                     <form>
-                        <input class="form-control" type="text" placeholder="Tên người dùng...." aria-label="default input example" />
-                        <input onChange={(e) => getValue1(e)} class="form-control" type="text" placeholder="Tên đăng nhập...." aria-label="default input example" />
-                        <input onChange={getValue2} class="form-control" type="text" placeholder="Mật khẩu...." aria-label="default input example" />
-                        <input class="form-control" type="text" placeholder="Xác nhận mật khẩu...." aria-label="default input example" />
+                        <input name='name' onChange={(e, name) => getValue(e, 'name')} class="form-control" type="text" placeholder="Tên người dùng...." aria-label="default input example" />
+                        <input name='email' onChange={(e) => getValue(e, 'email')} class="form-control" type="text" placeholder="Email đăng nhập...." aria-label="default input example" />
+                        <input onChange={(e) => getValue(e, 'password')} class="form-control" type="text" placeholder="Mật khẩu...." aria-label="default input example" />
+                        <input onChange={(e) => getValue(e, 'confirmPassword')} class="form-control" type="text" placeholder="Nhập lại mật khẩu...." aria-label="default input example" />
+                        <input onChange={(e) => getValue(e, 'address')} class="form-control" type="text" placeholder="Địa chỉ...." aria-label="default input example" />
+                        <input onChange={(e) => getValue(e, 'phonenumber')} class="form-control" type="text" placeholder="Sdt..." aria-label="default input example" />
 
+                        <span>{stateSignupMessage}</span>
                         <div className="d-grid gap-2">
                             <button onClick={submitAction} class="btn" type="button">Đăng nhập</button>
                         </div>
-                        <div>
-                            <button onClick={obj.switchForm} type="button">Đăng nhập</button>
+                        <div className={cv('block-login2')}>
+                            <a href='/'>Quên mật khẩu?</a>
+                            <a href='/'>Dăng nhập với SMS</a>
+                        </div>
+                        <div className={cv('block-border')}>
+                            <div className={cv('block-border-2')}></div>
+                            <div className={cv('block-border-label')}>HOẶC</div>
+                            <div className={cv('block-border-2')}></div>
+                        </div>
+                        <div className={cv('wrap-icon-login')}>
+                            <span>
+                                <a href='/'>
+                                    <i class="fa-brands fa-facebook"></i>
+                                    <span>Facebook</span>
+                                </a>
+                            </span>
+                            <span>
+                                <a href='/'>
+
+                                    <i class="fa-brands fa-google"></i>
+                                    <span> Google</span>
+                                </a>
+                            </span>
+                            <span>
+                                <a href='/'>
+                                    <i class="fa-brands fa-apple"></i>
+                                    <span>Apple</span>
+                                </a>
+                            </span>
+                        </div>
+                        <div className={cv('block-signup')}>
+                            bạn mới biết đến Shopee?
+                            <a onClick={obj.switchForm} type="button"> Đăng ký</a>
                         </div>
                     </form>
                 </div>
@@ -56,4 +123,4 @@ function SignUp(obj) {
     );
 }
 
-export default SignUp;
+export default SignIn;

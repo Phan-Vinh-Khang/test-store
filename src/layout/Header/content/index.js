d502f100fadbc94786c0e6632dcfc6a65d23021c
Phan Vĩnh Khang <khangpv.19th@sv.dla.edu.vn>
2024-06-18 03:14:05 +0700

    update select options

--- src/layout/Header/content/index.js
+++ src/layout/Header/content/index.js
@@ -10,9 +10,10 @@ import { setSearch } from '../../../redux/reduxSearch';
 import Select from 'react-select';
 let cv = classNames.bind(objStyle)
 let cv2 = classNames.bind(objGlobalStyle)
-const options = storage('getItem', 'search')
+
 function ContentHeader() {
     let [stateInput, setStateInput] = useState('');
+    let [options, setStateOptions] = useState(storage('getItem', 'search'))
     let dispatch = useDispatch();
     console.log('reload', stateInput)
     return (
@@ -28,49 +29,43 @@ function ContentHeader() {
                 <div style={{ width: '100%' }}>
                     <Select
                         inputValue={stateInput}
-                        onChange={({ value }) => {
-                            setStateInput(value);
-                        }}
+                        onChange={({ value }) => dispatch(setSearch(value))}
                         onKeyDown={(e) => {
                             if (e.key == 'Enter') {
-                                // console.log(stateInput, 'onKeyDown')
-                                let isExist = options.some((item) => item.value == stateInput)
-                                storage('setItem', 'search', { label: stateInput, value: stateInput })
-                                options.push({ label: stateInput, value: stateInput })
-                                console.log(isExist, 'isExist')
-                                if (isExist) {
-                                    let idxItemExist = options.findIndex((item) => item.value == stateInput)
-                                    storage('D-item', 'search', '', idxItemExist);
-                                    options.splice(idxItemExist, 1);
+                                const isExistIndex = options.findIndex((item) => item.value == stateInput);
+                                storage('setItem', 'search', { value: { label: stateInput, value: stateInput } });
+                                options.push({ label: stateInput, value: stateInput });
+                                if (isExistIndex >= 0) {
+                                    storage('D-item', 'search', { idx: isExistIndex });
+                                    options.splice(isExistIndex, 1);
                                 }
-                                // if (options.length >= 5) {
-                                //     storage('D-item', 'search', '', 0);
-                                //     options.shift();
-                                // }
-                                dispatch(setSearch(stateInput))
+                                if (options.length > 5) {
+                                    storage('D-item', 'search', { idx: 0 });
+                                    options.shift();
+                                }
+                                setStateOptions([...options]);
+                                dispatch(setSearch(stateInput));
                             }
 
                         }}
                         onInputChange={(data, { action }) => {
-                            if (action == 'input-change') {
-                                setStateInput(data);
+                            if (action == 'input-change') setStateInput(data);
+                            if (action == 'menu-close' && stateInput != '') {
+                                const isExistIndex = options.findIndex((item) => item.value == stateInput)
+                                storage('setItem', 'search', { value: { label: stateInput, value: stateInput } })
+                                options.push({ label: stateInput, value: stateInput })
+                                if (isExistIndex >= 0) {
+                                    storage('D-item', 'search', '', { idx: isExistIndex });
+                                    options.splice(isExistIndex, 1);
+                                }
+                                if (options.length > 5) {
+                                    storage('D-item', 'search', { idx: 0 });
+                                    options.shift();
+                                }
+                                setStateOptions([...options]);
                             }
-                            // if (action == 'menu-close' && stateInput != '') {
-                            //     storage('setItem', 'search', { label: stateInput, value: stateInput })
-                            //     options.push({ label: stateInput, value: stateInput })
-                            //     let isExist = options.some((item) => item.value == stateInput)
-                            //     if (isExist) {
-                            //         let idxItemExist = options.findIndex((item) => item.value == stateInput)
-                            //         storage('D-item', 'search', '', idxItemExist);
-                            //         options.splice(idxItemExist, 1);
-                            //     }
-                            //     // if (options.length >= 5) {
-                            //     //     storage('D-item', 'search', '', 0);
-                            //     //     options.shift();
-                            //     // }
-                            // }
                         }}
-                        options={options}
+                        options={[...options].reverse()}
                         noOptionsMessage={() => null} //khi ko có bất kì option nào dc tìm thấy khi search function này sẽ dc call
                     />
                 </div>
@@ -100,17 +95,25 @@ function ContentHeader() {
         </div >
     );
 }
-function storage(action, key, value = '', idx = -1) {
-    let data = JSON.parse(localStorage.getItem(key)) || [];
+function storage(action, key, { value, idx } = {}) {
+    /*phải set 1 object {} nếu ko đưa đối số thứ 3 vào
+    nếu ko có đối số thứ 3 tham số object này reference vào undefine và undefine thì ko có prop value hoặc idxl
+    ko thể sử dụng undefined.value hoặc undefined.idx nên sẽ báo lỗi
+    */
+    let data;
     switch (action) {
         case 'getItem':
-            return JSON.parse(localStorage[action](key)) || [];
+            return (JSON.parse(localStorage[action](key))) || [];
         case 'setItem':
+            data = storage('getItem', key); //return arr object
             data.push(value);
-            localStorage[action](key, JSON.stringify(data))
+            localStorage[action](key, JSON.stringify(data));
+            break;
         case 'D-item':
+            data = storage('getItem', key);
             data.splice(idx, 1);
-            localStorage.setItem(key, JSON.stringify(data))
+            localStorage.setItem(key, JSON.stringify(data));
+            break;
     }
 }
 export default ContentHeader;
